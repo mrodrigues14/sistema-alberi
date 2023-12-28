6b15e2da9b1a835fb1a73d9cfabd883855bb39cd
Matheus Rodrigues <rmatheus440@gmail.com>
2023-12-27 22:51:19 -0300

    Adicionar template padronizado de menu

--- backend/src/base/server.js
+++ backend/src/base/server.js
@@ -2,16 +2,30 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
-const routing = require('../routing')
+const routing = require('../routing');
+const session = require('express-session');
 
+// Configuração do express-session
+app.use(session({
+    secret: '1234',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        secure: false,
+        maxAge: 24 * 60 * 60 * 1000
+    }
+}));
 
-app.use(express.static(path.join(__dirname, '../../../frontend/paginaLogin')));
+app.use(express.static(path.join(__dirname, '../../../frontend')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.redirect('/paginaLogin/index.html');
+});
 
 routing(app);
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
 });
-
