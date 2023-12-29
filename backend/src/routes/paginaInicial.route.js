5fa992d9d230cbe1b2edcad6e7703d3d8e473ba1
Matheus Rodrigues <rmatheus440@gmail.com>
2023-12-29 20:53:12 -0300

    add query para buscar empresas no menu inicial

--- backend/src/routes/paginaInicial.route.js
+++ backend/src/routes/paginaInicial.route.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const db = require('../base/database');
+
+let nomeEmpresaConsulta = '';
 
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../../../frontend/paginaInicial/paginaInicial.html'));
+});
+
+router.post('/consultarEmpresas', (req, res) => {
+    db.query('SELECT nome FROM cliente', (error, results) => {
+        if (error) {
+            console.error('Erro durante a busca por empresa no banco de dados:', error);
+            return res.status(500).send('Server error');
+        }
+
+        if (results.length > 0) {
+            // Enviar todos os nomes encontrados
+            res.json(results.map(result => result.nome));
+        } else {
+            res.status(404).send({ message: 'Nenhuma empresa encontrada' });
+        }
+    });
 });
 
 module.exports = router;
--- frontend/paginaInicial/paginaInicialScript.js
+++ frontend/paginaInicial/paginaInicialScript.js
@@ -19,3 +19,58 @@ document.addEventListener('DOMContentLoaded', function() {
             console.error('Erro ao carregar o template:', error);
         });
 });
+
+
+function loadNomeEmpresa() {
+    fetch('/paginainicial/consultarEmpresas', { method: 'POST' })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Não foi possível buscar as empresas');
+            }
+            return response.json();
+        })
+        .then(data => {
+            var names = data;
+            initializeInput(names);
+        })
+        .catch(error => {
+            console.error('Erro na requisição:', error);
+        });
+}
+
+function initializeInput(names) {
+    var input = document.getElementById('searchInput');
+    var list = document.getElementById('nameList');
+
+    input.addEventListener('input', function(e) {
+        updateList(e.target.value);
+    });
+
+    document.querySelector('.arrow-button').addEventListener('click', function() {
+        updateList('');
+    });
+
+    list.addEventListener('click', function(e) {
+        if (e.target.tagName === 'LI') {
+            input.value = e.target.textContent;
+            list.style.display = 'none';
+        }
+    });
+
+    function updateList(filter) {
+        var filteredNames = names.filter(function(name) {
+            return name.toLowerCase().includes(filter.toLowerCase());
+        });
+
+        list.innerHTML = '';
+        filteredNames.forEach(function(name) {
+            var li = document.createElement('li');
+            li.textContent = name;
+            list.appendChild(li);
+        });
+
+        list.style.display = filteredNames.length ? 'block' : 'none';
+    }
+}
+
+
