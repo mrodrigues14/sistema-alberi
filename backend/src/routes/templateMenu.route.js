db023b4c3a3b2af6370bf8e92eb452785aef686f
Matheus Rodrigues <rmatheus440@gmail.com>
2024-04-01 17:55:03 -0300

    update

--- backend/src/routes/templateMenu.route.js
+++ backend/src/routes/templateMenu.route.js
@@ -6,4 +6,14 @@ router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../../../frontend/templateMenu/template.html'));
 });
 
+app.get('/logout', function(req, res){
+    req.session.destroy(function(err) {
+        if(err) {
+            console.log(err);
+        } else {
+            res.redirect('/api/login');
+        }
+    });
+});
+
 module.exports = router;
