567692148147279988aef4fab4413f6c7165a4ea
Matheus Rodrigues <rmatheus440@gmail.com>
2024-04-01 17:36:11 -0300

    update

--- backend/src/base/server.js
+++ backend/src/base/server.js
@@ -3,7 +3,7 @@ const https = require('https');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = 8080;
 const routing = require('../routing');
 const session = require('express-session');
 
