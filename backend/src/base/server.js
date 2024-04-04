ffe00a1610219f065b9b6e3ba6f20812f5a53363
Matheus Rodrigues <102708746+mrodrigues14@users.noreply.github.com>
2024-04-04 03:25:20 -0300

    Update server.js

--- backend/src/base/server.js
+++ backend/src/base/server.js
@@ -6,22 +6,19 @@ const app = express();
 const port = 8080;
 const routing = require('../routing');
 const session = require('express-session');
-const {createServer} = require("http");
+const {createServer} = require("https");
 
-// Localização dos arquivos de certificado
-// const options = {
-//
-//     key: fs.readFileSync('/etc/letsencrypt/live/albericonsult.com.br/privkey.pem'),
-//     cert: fs.readFileSync('/etc/letsencrypt/live/albericonsult.com.br/fullchain.pem'),
-// };
+const options = {
+    key: fs.readFileSync('/etc/letsencrypt/live/albericonsult.com.br/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/albericonsult.com.br/fullchain.pem'),
+};
 
-// Configuração do express-session
 app.use(session({
     secret: '1234',
     resave: false,
     saveUninitialized: true,
     cookie: {
-        secure: true, // Atualize esta linha para true para suporte HTTPS
+        secure: true, 
         maxAge: 24 * 60 * 60 * 1000
     }
 }));
@@ -31,7 +28,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json({limit: '50mb'}));
 routing(app);
 
-// Criando um servidor HTTPS em vez de usar app.listen
-createServer(app).listen(port, () => {
+createServer(options, app).listen( port, () => {
     console.log(`Server running on https://localhost:${port}`);
 });
