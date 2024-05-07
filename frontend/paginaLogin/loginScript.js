912f9e5f085cb1e18f2a26a000a769f4ed56c29f
Matheus Rodrigues <rmatheus440@gmail.com>
2024-05-07 19:00:15 -0300

    updates

--- frontend/paginaLogin/loginScript.js
+++ frontend/paginaLogin/loginScript.js
@@ -1,23 +1,56 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const loginForm = document.querySelector('form');
 
+    loginForm.addEventListener('submit', function (e) {
+        e.preventDefault();
+        const username = document.querySelector('input[name="username"]').value;
+        const password = document.querySelector('input[name="password"]').value;
 
-function buscarIdUsuarioApi(username) {
-    fetch('/login/buscarIdUsuario', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ username: username})
-    })
-        .then(response => response.json())
-        .then(data => {
-            if (data.success && data.idusuario) {
-                localStorage.setItem('userId', data.idusuario);
-                window.location.href = '/paginaInicial';
-            } else {
-                console.error('Usuário não encontrado ou erro na requisição.');
-            }
+        fetch('/api/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                username: username,
+                password: password
+            })
         })
-        .catch(error => {
-            console.error('Erro na requisição para obter o ID do usuário:', error);
-        });
+            .then(response => response.json())
+            .then(data => {
+                if (data.message === 'Login successful') {
+                    localStorage.setItem('username', data.user.username);
+                    localStorage.setItem('userRole', data.user.role);
+                    localStorage.setItem('idusuario', data.user.idusuario);
+
+                    window.location.href = '/paginaInicial';
+                } else {
+                    showModalError(data.message);
+                }
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+                showModalError('Ocorreu um erro ao tentar fazer login. Por favor, tente novamente mais tarde.');
+            });
+    });
+});
+
+function showModalError(message) {
+    const modal = document.getElementById('errorModal');
+    const span = document.getElementsByClassName('close')[0];
+    const errorMessage = document.getElementById('errorMessage');
+
+    errorMessage.textContent = message;
+
+    modal.style.display = "block";
+
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
 }
